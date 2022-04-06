9be6d1d7c54ed2482f06ecf408a1f8010f472a9b
CameronTwiggs <cameron.020617@gmail.com>
2022-04-06 05:15:19 -0400

    loaded lodash

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var _ = require('lodash');
+
+
+
+
+
 const x = 12;
 const y = 500;
 const z = x + y; 
@@ -16,3 +22,4 @@ console.log("Done");
 
 console.log("These changes are made from the other branch");
 
+console.log(_.partition([1, 2, 3, 4], n => n % 2));
